d986c8b21505ef93a3e9347fc73f813cb583b7a4
Scottie <scottie@cccareers.org>
2022-06-21 14:39:24 -0700

    Movie Finder

--- server/app.js
+++ server/app.js
@@ -1,8 +1,32 @@
 const express = require('express');
-
-
+const axios = require("axios");
+const morgan = require("morgan");
 const app = express();
 
-// When making calls to the OMDB API make sure to append the '&apikey=8730e0e' parameter
+require("dotenv").config();
+
+app.use(morgan("dev"));
+
+const APIkey = process.env.API_KEY;
+
+let arr = [];
+
+app.get("/", (req, res) => {
+    let key = Object.keys(req.query).toString();
+    let searchBy = key == "i" ? "imdbID" : "Title";
+    let found = arr.find(movie => movie[searchBy].toLowerCase() == req.query[key]);
+    if (found == undefined){
+        axios.get(`http://www.omdbapi.com${req.url}&apikey=${APIkey}`)
+        .then((movie) => {
+            res.send(movie.data);
+            arr.push(movie.data);
+        })
+        .catch((err) => {
+            console.log(err);
+        })
+    } else {
+        res.send(found);
+    }       
+}) 
 
 module.exports = app;
